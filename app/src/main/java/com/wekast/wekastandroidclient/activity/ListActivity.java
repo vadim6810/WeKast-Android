7fe30940d8428ef57af9690f5a57581d5df2f6af
Yehuda <yehudabukhantsov@gmail.com>
2016-08-03 14:47:33 +0300

    open active point on press buttonn Connect

--- app/src/main/java/com/wekast/wekastandroidclient/activity/ListActivity.java
+++ app/src/main/java/com/wekast/wekastandroidclient/activity/ListActivity.java
@@ -1,27 +1,39 @@
 package com.wekast.wekastandroidclient.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.wekast.wekastandroidclient.R;
+import com.wekast.wekastandroidclient.model.WifiApControl;
 
 /**
  * Created by Meztiros on 31.07.2016.
  */
 public class ListActivity extends Activity {
-
+    Context context;
+    WifiApControl apControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        // получаем экземпляр элемента ListView
-        ListView listView = (ListView) findViewById(R.id.listView);
 
+        initViewElements();
+    }
+
+    public void btnWelcome_Click(View view) {
+        Intent i = new Intent(ListActivity.this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    private void initViewElements() {
         // определяем массив типа String
         final String[] catNames = new String[]{
                 "Презентация1", "Презентация2", "Презентация3", "Презентация4", "Презентация5",
@@ -29,16 +41,28 @@ public class ListActivity extends Activity {
                 "Презентация11", "Презентация12", "Презентация13"
         };
 
-        // используем адаптер данных
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, catNames);
+        // получаем экземпляр элемента ListView
+        ListView listView = (ListView) findViewById(R.id.listView);
 
+        // используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, catNames);
         listView.setAdapter(adapter);
+
+        context = this;
+        apControl = new WifiApControl((WifiManager) this.getSystemService(Context.WIFI_SERVICE));
+
+        Button buttonConnect = (Button) findViewById(R.id.connectButton);
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createWiFiHotspot();
+            }
+        });
     }
 
-
-    public void btnWelcome_Click(View view) {
-        Intent i = new Intent(ListActivity.this, LoginActivity.class);
-        startActivity(i);
+    private void createWiFiHotspot() {
+        // TODO: add to SharedPreferences dongle IP
+        apControl.configureWifiConfig(getText(R.string.ssid).toString(), getText(R.string.pass).toString());
+        apControl.turnOnOffHotspot(context, true, apControl);
     }
 }
