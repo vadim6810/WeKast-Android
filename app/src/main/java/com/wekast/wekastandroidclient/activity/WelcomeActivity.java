7a4fadc8f1dc91ed6b6366eb2e0b74e4b8c383a0
Denys Romanchenko <6104172@gmail.com>
2016-08-19 00:30:50 +0300

    + welcome ver1

--- app/src/main/java/com/wekast/wekastandroidclient/activity/WelcomeActivity.java
+++ app/src/main/java/com/wekast/wekastandroidclient/activity/WelcomeActivity.java
@@ -3,7 +3,6 @@ package com.wekast.wekastandroidclient.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,18 +12,18 @@ import com.wekast.wekastandroidclient.model.AccessServiceAPI;
 import com.wekast.wekastandroidclient.R;
 import com.wekast.wekastandroidclient.model.Utils;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by RDL on 15.07.2016.
  */
 public class WelcomeActivity extends Activity {
     private TextView tvWelcome;
-    private ArrayList<String> filesServer = new ArrayList<>();
     private ArrayList<String> filesLocal = new ArrayList<>();
-    private HashMap<String, String> mapList = new HashMap<>();
+    private HashMap<String, String> mapDownload = new HashMap<>();
     Context context = this;
     AccessServiceAPI m_AccessServiceAPI;
     private static long back_pressed;
@@ -35,25 +34,20 @@ public class WelcomeActivity extends Activity {
         setContentView(R.layout.activity_welcome);
         tvWelcome = (TextView) findViewById(R.id.tv_welcome);
         tvWelcome.setText("Welcome: " + Utils.getFieldSP(context, "login"));
+        m_AccessServiceAPI = new AccessServiceAPI();
+        ListView presenterList = (ListView) findViewById(R.id.presenterList);
 
         String answer = getIntent().getStringExtra("answer");
 
-        //для адаптера (возможно будет убрано)
-        filesServer = Utils.parseJSONArray(context, answer);
-        filesLocal =  Utils.getAllFilesLocal();
-
         //для загрузки
-        mapList = Utils.parseJSONArrayMap(context, answer);
+        mapDownload = Utils.parseJSONArrayMap(context, answer);
 
-        // получаем элемент ListView
-        ListView presenterList = (ListView) findViewById(R.id.presenterList);
-
-        // создаем адаптер
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, filesServer);
+        //для адаптера
+        filesLocal =  Utils.getAllFilesLocal();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, filesLocal);
-
-        // устанавливаем для списка адаптер
         presenterList.setAdapter(adapter);
+
+        mappingPresentations();
     }
 
     @Override
@@ -73,8 +67,21 @@ public class WelcomeActivity extends Activity {
     public void btnDownload_Click(View v) {
         String login = Utils.getFieldSP(context, "login");
         String password = Utils.getFieldSP(context, "password");
-        if(mapList.size() > 0)
-        m_AccessServiceAPI.taskDownload(login, password, mapList, context);
+        if(mapDownload.size() > 0)
+        m_AccessServiceAPI.taskDownload(login, password, mapDownload, context);
         else Utils.toastShow(context, "You havn't presentations on server!");
+    }
+
+    private void mappingPresentations() {
+        if (mapDownload.size() > 0) {
+            for (String s: filesLocal) {
+                for(Iterator<HashMap.Entry<String, String>> it = mapDownload.entrySet().iterator(); it.hasNext(); ) {
+                    HashMap.Entry<String, String> entry = it.next();
+                    if (entry.getValue().equals(s)) {
+                        it.remove();
+                    }
+                }
+            }
+        }
     }
 }
