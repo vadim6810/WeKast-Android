859035acdea62b945bd3264f7e14b57c4e14d4f0
Denys Romanchenko <6104172@gmail.com>
2016-09-10 19:08:22 +0300

    + add fr 3

--- app/src/main/java/com/wekast/wekastandroidclient/activity/FragmentListPresentations.java
+++ app/src/main/java/com/wekast/wekastandroidclient/activity/FragmentListPresentations.java
@@ -1,13 +1,16 @@
 package com.wekast.wekastandroidclient.activity;
 
+import android.app.Activity;
 import android.app.ListFragment;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,7 +20,9 @@ import com.wekast.wekastandroidclient.model.Utils;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.wekast.wekastandroidclient.model.Utils.*;
@@ -32,14 +37,23 @@ public class FragmentListPresentations  extends ListFragment implements SwipeRef
     private String login;
     private String password;
 
+    private List<RowItem> rowItems;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         login = getFieldSP(getActivity(), "login");
         password = getFieldSP(getActivity(), "password");
 
+        rowItems = new ArrayList<>();
+        ArrayList<String> local = getAllFilesLocal();
+        for (int i = 0; i < local.size(); i++) {
+            RowItem items = new RowItem(local.get(i), R.drawable.wekastbrand);
+            rowItems.add(items);
+        }
+
         initListPresentations();
-        new TaskDownload().execute(login, password);
+//        new TaskDownload().execute(login, password);
     }
 
     @Override
@@ -51,10 +65,15 @@ public class FragmentListPresentations  extends ListFragment implements SwipeRef
         return view;
     }
 
+//    private void initListPresentations() {
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, getAllFilesLocal());
+//        setListAdapter(adapter);
+//        adapter.notifyDataSetChanged();
+//    }
     private void initListPresentations() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, getAllFilesLocal());
+        CustomAdapter adapter = new CustomAdapter(getActivity(), rowItems);
         setListAdapter(adapter);
-        adapter.notifyDataSetChanged();
+//        adapter.notifyDataSetChanged();
     }
 
     public void onListItemClick(ListView l, View v, int position, long id) {
@@ -64,7 +83,6 @@ public class FragmentListPresentations  extends ListFragment implements SwipeRef
 
     @Override
     public void onRefresh() {
-//        initListPresentations();
         swipeRefreshLayout.setRefreshing(true);
         new TaskDownload().execute(login, password);
     }
@@ -126,5 +144,85 @@ public class FragmentListPresentations  extends ListFragment implements SwipeRef
             initListPresentations();
             swipeRefreshLayout.setRefreshing(false);
         }
+    }
+
+    public class RowItem {
+
+        private String title;
+        private int icon;
+
+        public RowItem(String title, int icon) {
+            this.title = title;
+            this.icon = icon;
+
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getIcon() {
+            return icon;
+        }
+
+        public void setIcon(int icon) {
+            this.icon = icon;
+        }
+
+    }
+
+    public class CustomAdapter extends BaseAdapter {
+
+        Context context;
+        List<RowItem> rowItem;
+
+        CustomAdapter(Context context, List<RowItem> rowItem) {
+            this.context = context;
+            this.rowItem = rowItem;
+
+        }
+
+        @Override
+        public int getCount() {
+
+            return rowItem.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+
+            return rowItem.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+
+            return rowItem.indexOf(getItem(position));
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+                convertView = mInflater.inflate(R.layout.fragment_listitem, null);
+            }
+
+            ImageView imgIcon = (ImageView) convertView.findViewById(R.id.preview);
+            TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
+
+            RowItem row_pos = rowItem.get(position);
+            // setting the image resource and title
+            imgIcon.setImageResource(row_pos.getIcon());
+            txtTitle.setText(row_pos.getTitle());
+
+            return convertView;
+
+        }
+
     }
 }
